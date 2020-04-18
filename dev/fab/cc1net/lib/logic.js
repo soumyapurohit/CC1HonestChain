8666d6713b31fce66af9c0de0bf946b7525ec927
bdytx5 <bdytx5@mail.missouri.edu>
2020-04-18 16:14:41 -0500

    biznetUpdates

--- dev/fab/cc1net/lib/logic.js
+++ dev/fab/cc1net/lib/logic.js
@@ -18,26 +18,12 @@
  */
 
 /**
- * Sample transaction
- * @param {org.example.biznet.SampleTransaction} sampleTransaction
+ * Track the trade of a commodity from one trader to another
+ * @param {org.example.mynetwork.Trade} trade - the trade to be processed
  * @transaction
  */
-async function sampleTransaction(tx) {
-    // Save the old value of the asset.
-    const oldValue = tx.asset.value;
-
-    // Update the asset with the new value.
-    tx.asset.value = tx.newValue;
-
-    // Get the asset registry for the asset.
-    const assetRegistry = await getAssetRegistry('org.example.biznet.SampleAsset');
-    // Update the asset in the asset registry.
-    await assetRegistry.update(tx.asset);
-
-    // Emit an event for the modified asset.
-    let event = getFactory().newEvent('org.example.biznet', 'SampleEvent');
-    event.asset = tx.asset;
-    event.oldValue = oldValue;
-    event.newValue = tx.newValue;
-    emit(event);
+async function tradeCommodity(trade) {
+    trade.commodity.owner = trade.newOwner;
+    let assetRegistry = await getAssetRegistry('org.example.mynetwork.Commodity');
+    await assetRegistry.update(trade.commodity);
 }
