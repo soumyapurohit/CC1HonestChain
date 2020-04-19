ae6224daca0e7ed65ad4cae615b2437c96a65491
bdytx5 <bdytx5@mail.missouri.edu>
2020-04-19 12:42:15 -0500

    netChanges

--- dev/fab/cc1net/lib/logic.js
+++ dev/fab/cc1net/lib/logic.js
@@ -19,7 +19,7 @@
 
 /**
  * Track the trade of a commodity from one trader to another
- * @param {org.example.biznet.Trade} trade - the trade to be processed
+ * @param {org.cc1.net.Trade} trade - the trade to be processed
  * @transaction
  */
 async function tradeCommodity(trade) {
