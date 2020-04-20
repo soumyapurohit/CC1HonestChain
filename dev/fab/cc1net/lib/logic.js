4bbd49c127efdfd5f92b711ddc55bf6772e8ed76
bdytx5 <bdytx5@mail.missouri.edu>
2020-04-19 20:40:14 -0500

    updates

--- dev/fab/cc1net/lib/logic.js
+++ dev/fab/cc1net/lib/logic.js
@@ -4,7 +4,22 @@
  * @transaction
  */
 async function trade(trade) {
+
+    var trustScore = (trade.ds + trade.cs)/2;
+    var risk;
+    if(trustScore <=3){
+        risk = -1; 
+    }else if(trustScore > 3 && trustScore <= 7){
+        risk = 0;
+    }else{
+        risk = 1; 
+    }
+
     trade.commodity.owner = trade.newOwner;
     let assetRegistry = await getAssetRegistry('org.cc1.net.Commodity');
-    await assetRegistry.update(trade.commodity);
+    if(risk == 1){
+        await assetRegistry.update(trade.commodity);
+    }else{
+        return {'trust':trust};
+    }
 }
